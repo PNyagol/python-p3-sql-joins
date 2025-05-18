b92beca3188c0cdf7d56ca0dbf4704dcf93a15b3
PNyagol <nyagolpeter@gmail.com>
2025-05-18 11:46:36 +0300

    uncomment

--- select.sql
+++ select.sql
@@ -1,17 +1,17 @@
 .mode column
 .width auto
 .headers on
--- SELECT * FROM cats WHERE owner_id = 1;
+SELECT * FROM cats WHERE owner_id = 1;
 
--- SELECT * FROM cat_owners
+SELECT * FROM cat_owners
 
--- SELECT cat_owners.owner_id
--- FROM cat_owners
--- WHERE cat_id =3;
+SELECT cat_owners.owner_id
+FROM cat_owners
+WHERE cat_id =3;
 
--- SELECT cat_owners.cat_id
--- FROM cat_owners
--- WHERE owner_id = 2;
+SELECT cat_owners.cat_id
+FROM cat_owners
+WHERE owner_id = 2;
 
 
 -- Advanced Querries
